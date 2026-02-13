124a2c727ff7b7cb37d9f0b4dadc5bbf4379e28a
ivankezic <thekezich@gmail.com>
2026-02-13 13:05:52 +0100

    .

--- 6. web shop admin/index.js
+++ 6. web shop admin/index.js
@@ -5,7 +5,13 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('index', {pageName: 'Products'})
+    res.render('index', { pageName: 'Products' })
+})
+app.get('/orders', (req, res) => {
+    res.render('orders', { pageName: 'Orders' })
+})
+app.get('/users', (req, res) => {
+    res.render('users', { pageName: 'Users' })
 })
 
 app.listen(3001)
