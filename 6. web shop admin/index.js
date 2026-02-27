1495b7ceef6dd3fb3f75ac58f2ba6f1301e5e3ec
ivankezic <thekezich@gmail.com>
2026-02-27 12:50:27 +0100

    .

--- 6. web shop admin/index.js
+++ 6. web shop admin/index.js
@@ -1,11 +1,41 @@
 import express from 'express'
+import mysql from 'mysql2'
+
+const dbConnection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'shop'
+})
+
+dbConnection.connect((error) => {
+    if (error) {
+        console.log('error 404 connecting to db:', error)
+    }
+})
 
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('index', { pageName: 'Products' })
+    dbConnection.query(
+        `select name, price, stock
+        from product;`,
+        (error, results, fields) => {
+            if (error) {
+                console.log('error executing query', error)
+                res.render("server-error")
+                return
+            }
+            console.log('simple select data', results)
+            res.render('index', { 
+                pageName: 'Products', 
+                products: results 
+            })
+        }
+    )
 })
 app.get('/orders', (req, res) => {
     res.render('orders', { pageName: 'Orders' })
